5225631297b6bc779bae5ae6798321ce08283c18
Emily Pham <emilyn3@hawaii.edu>
2023-11-16 01:35:32 -1000

    Implemented Edit Profile
    Created form and corresponding page for Edit Profile. Also required the implementation of Profile and updates to various related c
    omponents. Style has not been decided.

---
+++ app/imports/ui/pages/EditProfile.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import swal from 'sweetalert';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import { AutoForm, ErrorsField, HiddenField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import { useParams } from 'react-router';
+import LoadingSpinner from '../components/LoadingSpinner';
+import { Profiles } from '../../api/profiles/Profiles';
+
+const bridge = new SimpleSchema2Bridge(Profiles.schema);
+
+/* Renders the EditStuff page for editing a single document. */
+const EditProfile = () => {
+  // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
+  const { _id } = useParams();
+  // console.log('EditStuff', _id);
+  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+  const { doc, ready } = useTracker(() => {
+    // Get access to Stuff documents.
+    const subscription = Meteor.subscribe(Profiles.userPublicationName);
+    // Determine if the subscription is ready
+    const rdy = subscription.ready();
+    // Get the document
+    const document = Profiles.collection.findOne(_id);
+    return {
+      doc: document,
+      ready: rdy,
+    };
+  }, [_id]);
+  // console.log('EditStuff', doc, ready);
+  // On successful submit, insert the data.
+  const submit = (data) => {
+    const { username, image, membership, interests } = data;
+    Profiles.collection.update(_id, { $set: { username, image, membership, interests } }, (error) => (error ?
+      swal('Error', error.message, 'error') :
+      swal('Success', 'Item updated successfully', 'success')));
+  };
+
+  return ready ? (
+    <Container className="py-3">
+      <Row className="justify-content-center">
+        <Col xs={10}>
+          <Col className="text-center"><h2>Edit Profile</h2></Col>
+          <AutoForm schema={bridge} onSubmit={data => submit(data)} model={doc}>
+            <Card>
+              <Card.Body>
+                <Row>
+                  <Col><TextField name="username" /></Col>
+                </Row>
+                <Row>
+                  <Col><TextField name="image" /></Col>
+                  <Col><TextField name="membership" /></Col>
+                </Row>
+                <Row><SelectField name="interests" /></Row>
+                <SubmitField value="Submit" />
+                <ErrorsField />
+                <HiddenField name="ownerID" />
+              </Card.Body>
+            </Card>
+          </AutoForm>
+        </Col>
+      </Row>
+    </Container>
+  ) : <LoadingSpinner />;
+};
+
+export default EditProfile;
---
+++ app/imports/ui/pages/ViewProfile.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Profiles } from '../../api/profiles/Profiles';
+import LoadingSpinner from '../components/LoadingSpinner';
+import Profile from '../components/Profile';
+
+/* Renders a card with all Profile information. */
+const ViewProfile = () => {
+  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+  const { profiles, ready } = useTracker(() => {
+    // Get access to Stuff documents.
+    const subscription = Meteor.subscribe(Profiles.userPublicationName);
+    // Determine if the subscription is ready
+    const rdy = subscription.ready();
+    // Get the Profile documents
+    const prof = Profiles.collection.find({}).fetch();
+    return {
+      profiles: prof,
+      ready: rdy,
+    };
+  }, []);
+  return (ready ? (
+    <Container className="py-3">
+      <Row className="justify-content-center">
+        <Col>
+          <Col className="text-center">
+            <h2>My Profile</h2>
+          </Col>
+          <Row>
+            {profiles.map((profile, index) => <Col key={index}><Profile profile={profile} /></Col>)}
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  ) : <LoadingSpinner />);
+};
+
+export default ViewProfile;
