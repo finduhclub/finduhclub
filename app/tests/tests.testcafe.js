368995cc4a66300b15c113c444e6592862aae7e3
Emily Pham <99458185+empham@users.noreply.github.com>
2023-12-10 13:11:03 -1000

    Uncommented Other Tests

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -20,55 +20,55 @@ const credentialsAdmin = { username: 'admin@foo.com', password: 'changeme' };
 fixture('meteor-application-template-react localhost test with default db')
   .page('http://localhost:3000');
 
-// test('Test that landing page shows up', async (testController) => {
-//   await landingPage.isDisplayed(testController);
-// });
-//
-// test('Test that signin and signout work. Test home redirect.', async (testController) => {
-//   // For regular user
-//   await navBar.gotoSignInPage(testController);
-//   await signinPage.signin(testController, credentials.username, credentials.password);
-//   await navBar.isLoggedIn(testController, credentials.username);
-//   await homePage.isDisplayed(testController);
-//   await navBar.logout(testController);
-//   await signoutPage.isDisplayed(testController);
-// });
-//
-// test('Test that list clubs page displays', async (testController) => {
-//   await navBar.gotoSignInPage(testController);
-//   await signinPage.signin(testController, credentials.username, credentials.password);
-//   await navBar.gotoListClubsPage(testController);
-//   await listClubsPage.isDisplayed(testController);
-// });
-//
-// test('Test that view profile page displays', async (testController) => {
-//   await navBar.gotoSignInPage(testController);
-//   await signinPage.signin(testController, credentials.username, credentials.password);
-//   await navBar.gotoViewProfilePage(testController);
-//   await viewProfilePage.isDisplayed(testController);
-// });
-//
-// test('Test that manage clubs admin page displays', async (testController) => {
-//   await navBar.gotoSignInPage(testController);
-//   await signinPage.signin(testController, credentialsAdmin.username, credentialsAdmin.password);
-//   await navBar.gotoManageClubsPage(testController);
-//   await manageClubsPage.isDisplayed(testController);
-// });
-//
-// test('Test that manage profiles admin page displays', async (testController) => {
-//   await navBar.gotoSignInPage(testController);
-//   await signinPage.signin(testController, credentialsAdmin.username, credentialsAdmin.password);
-//   await navBar.gotoManageProfilesPage(testController);
-//   await manageProfilesPage.isDisplayed(testController);
-// });
-//
-// test('Test that edit profile page works', async (testController) => {
-//   await navBar.gotoSignInPage(testController);
-//   await signinPage.signin(testController, credentials.username, credentials.password);
-//   await navBar.gotoEditProfilePage(testController);
-//   await editProfilePage.isDisplayed(testController);
-//   await editProfilePage.editProfile(testController);
-// });
+test('Test that landing page shows up', async (testController) => {
+  await landingPage.isDisplayed(testController);
+});
+
+test('Test that signin and signout work. Test home redirect.', async (testController) => {
+  // For regular user
+  await navBar.gotoSignInPage(testController);
+  await signinPage.signin(testController, credentials.username, credentials.password);
+  await navBar.isLoggedIn(testController, credentials.username);
+  await homePage.isDisplayed(testController);
+  await navBar.logout(testController);
+  await signoutPage.isDisplayed(testController);
+});
+
+test('Test that list clubs page displays', async (testController) => {
+  await navBar.gotoSignInPage(testController);
+  await signinPage.signin(testController, credentials.username, credentials.password);
+  await navBar.gotoListClubsPage(testController);
+  await listClubsPage.isDisplayed(testController);
+});
+
+test('Test that view profile page displays', async (testController) => {
+  await navBar.gotoSignInPage(testController);
+  await signinPage.signin(testController, credentials.username, credentials.password);
+  await navBar.gotoViewProfilePage(testController);
+  await viewProfilePage.isDisplayed(testController);
+});
+
+test('Test that manage clubs admin page displays', async (testController) => {
+  await navBar.gotoSignInPage(testController);
+  await signinPage.signin(testController, credentialsAdmin.username, credentialsAdmin.password);
+  await navBar.gotoManageClubsPage(testController);
+  await manageClubsPage.isDisplayed(testController);
+});
+
+test('Test that manage profiles admin page displays', async (testController) => {
+  await navBar.gotoSignInPage(testController);
+  await signinPage.signin(testController, credentialsAdmin.username, credentialsAdmin.password);
+  await navBar.gotoManageProfilesPage(testController);
+  await manageProfilesPage.isDisplayed(testController);
+});
+
+test('Test that edit profile page works', async (testController) => {
+  await navBar.gotoSignInPage(testController);
+  await signinPage.signin(testController, credentials.username, credentials.password);
+  await navBar.gotoEditProfilePage(testController);
+  await editProfilePage.isDisplayed(testController);
+  await editProfilePage.editProfile(testController);
+});
 
 test('Test that add clubs admin page works', async (testController) => {
   await navBar.gotoSignInPage(testController);
